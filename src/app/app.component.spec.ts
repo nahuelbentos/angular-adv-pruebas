25da232b535007b46134092d4b3ebecd76c03205
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2020-08-31 12:55:24 -0300

    Fin proyecto de pruebas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet, RouterLink, RouterLinkWithHref } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
@@ -16,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pruebas'`, () => {
+  /*  it(`should have as title 'pruebas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pruebas');
@@ -26,6 +30,15 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pruebas app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'pruebas app is running!'
+    );
+  }); */
+
+  it('Debe de tener un router-oulet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const debugElement = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(debugElement).not.toBeNull();
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { AppComponent } from './app.component';
+import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { MedicoService } from './intermedio2/medico/medico.service';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
+import { RUTAS } from './avanzado/rutas/app.routes';
+import { NavbarComponent } from './avanzado/navbar/navbar.component';
+import { RouterMedicoComponent } from './avanzado/router-medico/router-medico.component';
 
 @NgModule({
-  declarations: [AppComponent, MedicosComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    MedicosComponent,
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent,
+    NavbarComponent,
+    RouterMedicoComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(RUTAS)],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
